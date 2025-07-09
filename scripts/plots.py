25b39108507351599d2936d98a13651963965e98
IgorMichels <igorxadrezbr@gmail.com>
2025-07-09 17:16:40 -0300

    change labels

--- scripts/plots.py
+++ scripts/plots.py
@@ -115,7 +115,9 @@ def create_acf_plots(
     n_cols = max(positions, key=lambda x: x[1])[1]
 
     fig_acf_all = sp.make_subplots(
-        rows=n_rows, cols=n_cols, subplot_titles=[f"Chain {c}" for c in range(n_chains)]
+        rows=n_rows,
+        cols=n_cols,
+        subplot_titles=[f"Chain {c+1}" for c in range(n_chains)],
     )
 
     for c, (row, col) in enumerate(positions):
@@ -128,7 +130,7 @@ def create_acf_plots(
                     mode="markers+lines",
                     line={"color": param_colors[k]},
                     marker={"color": param_colors[k]},
-                    name=f"$\\mu_{{{k}}}$",
+                    name=f"$\\mu_{{{k+1}}}$",
                     showlegend=(c == 0),
                 ),
                 row=row,
@@ -168,7 +170,9 @@ def create_histogram_plots(
     n_cols = max(positions, key=lambda x: x[1])[1]
 
     fig_hist_all = sp.make_subplots(
-        rows=n_rows, cols=n_cols, subplot_titles=[f"Chain {c}" for c in range(n_chains)]
+        rows=n_rows,
+        cols=n_cols,
+        subplot_titles=[f"Chain {c+1}" for c in range(n_chains)],
     )
 
     for c, (row, col) in enumerate(positions):
@@ -179,7 +183,7 @@ def create_histogram_plots(
                     nbinsx=30,
                     opacity=0.6,
                     marker={"color": param_colors[k]},
-                    name=f"$\\mu_{{{k}}}$",
+                    name=f"$\\mu_{{{k+1}}}$",
                     showlegend=(c == 0),
                 ),
                 row=row,
